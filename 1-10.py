0c03c9758b859bf0fb78961ca6773a3b810098ce
iAtharzaib <iatharzaib@gmail.com>
2019-05-14 18:52:39 +0500

    1-10 chapters

---
+++ 1-10.py
@@ -0,0 +1,109 @@
+
+print(' ---------------------Chapter 1--------------') 
+
+
+print('Hello World!')
+print('\n')
+
+print(' ---------------------Chapter 2--------------') 
+
+name="Hello"
+print(name)
+print('\n')
+
+print(' ---------------------chapter 3--------------') 
+
+num=150
+new_num=num-50
+print(new_num)
+print('\n')
+num=150
+new_num=num+50
+print(new_num)
+print('\n')
+num=150
+new_num=num/50
+print(new_num)
+print('\n')
+num=150
+new_num=num*50
+print(new_num)
+print('\n')
+print(' ---------------------chapter 4--------------') 
+
+pop=4
+popular_number=2+2
+print(pop)
+print(popular_number)
+print(pop+popular_number)
+print('\n')
+print(' ---------------------chapter 5--------------') 
+
+print('Legal names') 
+print('\n')
+print(' ---------------------chapter 6--------------') 
+
+modulus=100%3
+print(modulus)
+
+num=4
+den=3
+mul=5
+num+=4
+den-+3
+mul*=5
+num=num+54
+print(num)
+print(den)
+print(mul)
+print('\n')
+print(' ---------------------chapter 7--------------') 
+print('\n')
+total_cost=(1+3)*4
+total_cost2=1+(3*4)
+print(total_cost)
+print(total_cost2)
+
+print('\n')
+print(' ---------------------chapter 8--------------') 
+print('\n')
+message1='Greetings '
+message2='Everyone! '
+message3='& Welcome to the Presentation'
+print(message1+message2+message3)
+print('\n')
+
+print(' ---------------------chapter 9--------------') 
+print('\n')
+species='cat'
+if species=='cat':
+	print('Yep! its a cat')
+
+print('\n')
+print(' ---------------------chapter 10--------------') 
+
+number1=1234
+number11=1234
+number2=23
+number3=212
+if number11!=3245:
+	print('Sorry ! ')
+print('\n')
+if number2>number1:
+	print('number2 is bigger ! ')
+print('\n')
+
+if number2<number1:
+	print('number1 is bigger ')
+
+print('\n')
+if number1>=1230:
+	print('number1 is greater than equal to provided number ! ')
+
+print('\n')
+if number2!=3245:
+	print('Sorry ! number didnt match ')
+
+
+
+
---
+++ f1.py
@@ -0,0 +1,7 @@
+print("1st Lecture")
+
+print('*************************')
+print('Athar Zaib')
+print('Mazhar Ul Haque')
+print('Pakistan')
+print('Aritificial Intelligence')
